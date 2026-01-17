fd7ef1449771398502ddf947798eda59b66bdac8
axlrosas <axlrosas9902@gmail.com>
2026-01-16 18:08:05 -0600

    add a settings file

---
+++ finance_dashboard/lib/settings.py
@@ -0,0 +1,15 @@
+ACCOUNTS = ["BBVA", "Nu Turbo", "Nu 7.3", "Openbank", "Efectivo"]
+EXTERNAL = "EXTERNAL"
+
+DEFAULT_RATES = {
+    "BBVA": 0.0,
+    "Nu Turbo": 0.13,
+    "Nu 7.3": 0.073,
+    "Openbank": 0.10,
+    "Efectivo": 0.0,
+}
+
+DEFAULT_DAY_BASIS = 360
+
+TX_TYPES = ["income", "expense", "transfer"]
+CATEGORIES = []
